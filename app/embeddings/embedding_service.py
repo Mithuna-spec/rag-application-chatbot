68bf5a5435c0af23ba6c99a98b3052d124fafce8
Mithuna Thirunagari <thirunagarimithuna1@gmail.com>
2026-08-17 11:39:35 +0530

    Replace PyTorch embeddings with FastEmbed

--- app/embeddings/embedding_service.py
+++ app/embeddings/embedding_service.py
@@ -1,33 +1,9 @@
-# import os
-
-# from dotenv import load_dotenv
-# from langchain_google_genai import GoogleGenerativeAIEmbeddings
-
-# load_dotenv()
-
-
-# def get_embeddings():
-#     api_key = os.getenv("GOOGLE_API_KEY")
-
-#     if not api_key:
-#         raise ValueError(
-#             "GOOGLE_API_KEY is not set in the environment."
-#         )
-
-#     return GoogleGenerativeAIEmbeddings(
-#         model="gemini-embedding-001",
-#         google_api_key=api_key
-#     )
-
-
-from langchain_huggingface import HuggingFaceEmbeddings
-
-
 from functools import lru_cache
-from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
+
 
 @lru_cache(maxsize=1)
 def get_embeddings():
-    return HuggingFaceEmbeddings(
-        model_name="sentence-transformers/all-MiniLM-L6-v2"
+    return FastEmbedEmbeddings(
+        model_name="BAAI/bge-small-en-v1.5"
     )
